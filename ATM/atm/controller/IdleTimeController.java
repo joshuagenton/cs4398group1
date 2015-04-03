b5bbc02a0d18a90150bf8ddb10c49bf193bb7c18
atamido <atamido@ac098893-398e-1641-24fe-8c99a47cb302>
2015-04-03 01:54:47 +0000

    Fix the timer

--- ATM/atm/controller/IdleTimeController.java
+++ ATM/atm/controller/IdleTimeController.java
@@ -1,39 +1,33 @@
 package atm.controller;
 
-import java.awt.Toolkit;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class IdleTimeController extends AbstractController {
+public class IdleTimeController extends TimerTask {
 	
 	public IdleTimeController() {}
 	
-	Toolkit toolkit;
-	Timer timer;
+	//static Toolkit toolkit;
+	static Timer timer;
+	static TimerTask timerTask;
 
 	// If no events happen trigger timer.  
 	//TODO When we are idle, call runTimer(15) to start.
-	public void runTimer(int seconds) {
-	  
-		toolkit = Toolkit.getDefaultToolkit();
+	public static void runTimer(int seconds) {
+		timerTask = new IdleTimeController();
 		timer = new Timer();
-		//timer.schedule(idleLogout(), seconds * 1000);
+		timer.schedule(timerTask, seconds * 1000);
 	}
 
 	// If there is an action we need to cancel the timer.
 	public void cancelTimer() {
 		timer.cancel();
-	
 	}
 	
-  	public TimerTask idleLogout() {
-		
+  	public void run() {
   		// Log user out of the system and save changes.
   		MainController mc = new MainController();
   		mc.logout();
-  		
-  		// Not sure what we are supposed to return.
-  		return null;
     }
  }
 

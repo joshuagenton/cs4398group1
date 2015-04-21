63934660a5d7d567855cde4b5123a9e01adf4b84
stacielynn <stacielynn@ac098893-398e-1641-24fe-8c99a47cb302>
2015-04-21 16:32:54 +0000

    Comments for all the controllers.

--- ATM/atm/controller/Results.java
+++ ATM/atm/controller/Results.java
@@ -2,19 +2,38 @@ package atm.controller;
 
 import java.util.Comparator;
 
-
+/**
+ * This is the Results controller.  It is where the results of a user's
+ * transactions come from.
+ * 
+ * @author Chris Wells
+ * @since 2015-03-25
+ */
 public class Results{
 	
 	private String name;
 	private Integer accountNum;
 	private Double balance;
 	
+	/**
+	 * Default constructor.
+	 */
 	public Results(){}
+	
+	/**
+	 * Constructor.
+	 * @param n the name of the user
+	 * @param num the account number
+	 * @param b the balance of the account
+	 */
 	public Results(String n, Integer num, Double b){
 		name = n;
 		accountNum = num;
 		balance = b;
 	}
+	
+	
+	// Getters/Setters
 	
 	public String getName(){
 		return name;
@@ -26,9 +45,13 @@ public class Results{
 	public Double getBalance(){
 		return balance;
 	}
-	
-
 }
+
+/**
+ * The Comparator allows the system the avbility to compare two results.
+ * 
+ * @author Chris Wells
+ */
 class AccountCompare implements Comparator<Results> {
 	@Override
 	public int compare(Results o1, Results o2) {
